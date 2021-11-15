c2c1ec8d1dcb1c387c3ef885cb87625ce71757f8
fabiobr27 <fabio2000br@gmail.com>
2021-11-15 10:51:51 +0100

    EurekaServer

---
+++ MicroserviciosCliente/src/test/java/com/example/microServiciosClient/MicroserviciosClienteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.microServiciosClient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviciosClienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
